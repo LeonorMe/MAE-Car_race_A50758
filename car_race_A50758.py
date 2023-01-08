e73a774ab29da06383b94fa2d0e85f1d6cea3ec1
LeonorMe <leonomedeiros@gmail.com>
2023-01-08 23:03:50 +0000

    done

--- car_race_A50758.py
+++ car_race_A50758.py
@@ -3,20 +3,20 @@
 # é porque o pygame não está bem instalado
 import pygame, sys
 from pygame.locals import *
-from math import cos, sin, sqrt
-
+from math import cos, sin, atan, sqrt
+from PIL import Image
 
 # inicialização do módulo pygame
 pygame.init()
 
 # criação de uma janela
 largura = 937
-altura  = 696
+altura = 696
 tamanho = (largura, altura)
-janela  = pygame.display.set_mode(tamanho)
-pygame.display.set_caption('CarRace da Leonor') #nome da janela
-#Nesta janela o ponto (0,0) é o canto superior esquerdo
-#e (532-1,410-1) = (531,409) o canto inferior direito.
+janela = pygame.display.set_mode(tamanho)
+pygame.display.set_caption('CarRace da Leonor')  # nome da janela
+# Nesta janela o ponto (0,0) é o canto superior esquerdo
+# e (532-1,410-1) = (531,409) o canto inferior direito.
 
 
 # número de imagens por segundo
@@ -25,99 +25,184 @@ frame_rate = 24
 # relógio para controlo do frame rate
 clock = pygame.time.Clock()
 
+########################
+#Musica
+pygame.mixer.init()
+motor = pygame.mixer.Sound("motor.mp3")
+sirene = pygame.mixer.Sound("sirene.wav")
+
 # ler uma imagem em formato bmp
 pista = pygame.image.load("circuitoA50758.jpg")
 carro = pygame.image.load("carroA50758.jpg")
+#------------
+policia = pygame.image.load("policiaA50758.jpg")
+policia = pygame.transform.scale(policia, (30,30))
+#-----------
 
-    
-#Inicializa o tempo
-t=0.0
-
+# Inicializa o tempo
+t = 0.0
+motor.play()
 
 #########################
-#Para escrever o tempo:
-font_size = 25
-font = pygame.font.Font(None, font_size) # fonte pré-definida
-antialias = True # suavização
-WHITE = (255, 255, 255)# cor (terno com os valores Red, Green, Blue entre 0 e 255)
-BLACK = (5, 60, 5)
+# Para escrever o tempo:
+font_size = 35
+font = pygame.font.Font(None, font_size)  # fonte pré-definida
+antialias = True  # suavização
+WHITE = (255, 255, 255)  # cor (terno com os valores Red, Green, Blue entre 0 e 255)
+BLACK = (5, 30, 5)
 
 ##################################
 ##Exemplo ajustado à pista
 
-def parametrizacao (t): # as curvas tem metade da velocidade das retas
-    if t>=0:
-        resultado = (0,370) # Posição incial
-    if 0<t<=1:
-        resultado = (0+500*t, 370 - 45*t) # primeira reta (em contra-mão)
-    if 1 < t <= 5.14: # primeira volta para sair (em contra-mão)
-        resultado = (500 - 100 * cos(0.57 + t), 325 + 100 * sin(-1 + t))
-    #if 2 < t <= 2.5: # segunda reta para mudar de sentido
-     #   resultado = (347.4 - 250 * (t - 3.64), 339.6)
+def parametrizacao(t):  # as curvas tem metade da velocidade das retas
+    vreta = 250 # velocidade retas
+    vcurva = vreta/2 # velocidade curvas
+    if t >= 0:
+        resultado = (0, 375) # (0,375)
+    if 0 < t <= 2.20:
+        resultado = (0 + 249 * t, 375 + -22 * t) # pf = (523,329)
+    if 2.20 < t <= 4.9:
+        raio = 70
+        resultado = (533 + 70 * cos(1+t*vcurva/raio), 249 - 70 * sin(1+t*vcurva/raio)) # pf = (479,271)
+    if 4.9 < t <= 5.1:
+        ti = 4.9
+        pix = 479 - 15
+        piy = 271
+        resultado = (pix - (t-ti), piy + vreta * (t-ti)) # pf = (474,425)
+    if 5.1 < t <= 5.2:
+        print("A passar por debaixo da estrada")
+        #carro a passar por debaixo (477,332) a (481,377)
+    if 5.2 < t <= 5.5:
+        ti = 4.9
+        pix = 479 - 15
+        piy = 271
+        resultado = (pix - (t - ti), piy + vreta * (t - ti))  # pf = (474,425)
+    if 5.5 < t <= 8.3:
+        raio = 70
+        pix = 474 + 60
+        piy = 425
+        fi = -0.5
+        resultado = (pix + 70 * cos(fi+t*vcurva/raio), piy - 70 * sin(fi+t*vcurva/raio)) # pf = (525,360)
+    if 8.3 < t <= 10.5:
+        ti = 8.3
+        (pix,piy) = (525,360-5)
+        resultado = (pix - 249 * (t-ti), piy + 22 * (t-ti)) # pf = (523,329)
+        #print(resultado)
+    if 10.5 < t:
+        resultado = (0, 0)
     return resultado
 
-'''
-    if 0.5<t<=3.64:
-        resultado=(355+110*cos(-2+t),190+150*sin(-2+t))
-    if 3.64<t<=4:
-        resultado=(347.4-250*(t-3.64), 339.6)
-    if 4<t<=4.4:
-        resultado=(250, 340-250*(t-4))
-    if 4.4<t<=4.8:
-        resultado=(250+250*(t-4.4), 230)
-    if 4.8<t<=5.2:
-        resultado=(355, 230-250*(t-4.8))
-    if 5.2<t<=5.6:
-        resultado=(355-500*(t-5.2), 120)#cerca do dobro da velocidade
-    if 5.6<t<=5.9:
-        resultado=(160, 140+500*(t-5.6))#cerca do dobro da velocidade
-    if 5.9<t<=6.4:
-        resultado=(120+450*(t-5.9), 340-(450*(t-5.9)**2))#cerca do dobro da velocidade
-    if t>6.4:
-        resultado=(0,0)
-'''
+def parametrizacao_policia(t):
+    if t < 9:
+        resultado2 = (0, 0) # (932,275)
+    if t == 9:
+        resultado2 = (0, 0)
+    if 9 < t <= 15:
+        sirene.play()
+        ti = 9
+        (pix, piy) = (940, 270)
+        resultado2 = (pix - 220 * (t - ti), piy + 25 * (t - ti))  # pf = (523,329)
+    if t > 15:
+        pygame.mixer.pause()
+        resultado2 = (0, 0)
+    return resultado2
+
+def carro_rotacao(t):
+    vreta = 250  # velocidade retas
+    vcurva = vreta / 2  # velocidade curvas
+    if t >= 0:
+        (velx, vely) = (0, 0)
+    if 0 < t <= 2.20:
+        (velx, vely) = (249 , -22)
+    if 2.20 < t <= 4.9:
+        raio = 70
+        (velx, vely) = ( -sin(1 + t * vcurva / raio) * vcurva, - cos(1 + t * vcurva / raio) * vcurva)
+    if 4.9 < t <= 5.5:
+        (velx, vely) = (-1, vreta)
+    if 5.5 < t <= 8.3:
+        raio = 70
+        fi = -0.5
+        (velx, vely) = (- cos(fi + t * vcurva / raio) * vcurva, - cos(fi + t * vcurva / raio) * vcurva)
+    if 8.3 < t <= 10.5:
+        (velx, vely) = (249, 22)
+    if 10.5 < t:
+        (velx, vely) = (0, 0)
+
+    if velx > 0:
+        angulo = atan(vely / velx)
+    elif velx < 0:
+        angulo = atan(vely / velx) + 3.14159
+    else:
+        angulo = 0
+
+    return pygame.transform.rotate(carro, angulo)
+
+def modulo(x,y):
+    return sqrt(x*x + y*y)
+
+def veloc(t):
+    vreta = 250  # velocidade retas
+    vcurva = vreta / 2  # velocidade curvas
+    if t >= 0:
+        mod_vel = 0
+    if 0 < t <= 2.20:
+        mod_vel = modulo(249, -22)
+    if 2.20 < t <= 4.9:
+        raio = 70
+        mod_vel = modulo(-sin(1 + t * vcurva / raio) * vcurva, - cos(1 + t * vcurva / raio) * vcurva)
+    if 4.9 < t <= 5.5:
+        mod_vel = modulo(-1, vreta)
+    if 5.5 < t <= 8.3:
+        raio = 70
+        mod_vel = modulo(- sin(-0.5 + t * vcurva / raio) * vcurva, - cos(-0.5 + t * vcurva / raio) * vcurva)
+    if 8.3 < t <= 10.5:
+        mod_vel = modulo(249, 22)
+    if 10.5 < t:
+        mod_vel = 0
+
+    return round(mod_vel,3)
 
 #######################
 
-#(A) Se descomentar aqui (e comentar B) vejo onde passou/ rasto da trajetória
+# (A) Se descomentar aqui (e comentar B) vejo onde passou/ rasto da trajetória
 # Pois neste caso só junta a pista uma vez,
-#no outro caso está sempre a juntar/desenhar a pista
-#janela.blit(pista, (0, 0)) 
+# no outro caso está sempre a juntar/desenhar a pista
+#janela.blit(pista, (0, 0))
 
 #################################
-#Ciclo principal do jogo
+# Ciclo principal do jogo
 while True:
-    tempo = font.render("t="+str(int(t)), antialias, BLACK)
-    velocidade = font.render("v="+str(int(t)), antialias, BLACK)
-    janela.blit(pista, (0, 0))  #(B) se descomentar aqui (e comentar (A)) vejo movimento
-    janela.blit(carro, parametrizacao(t))
-    janela.blit(tempo, (10, 10))
-    janela.blit(velocidade, (10, 30)) #Mostrar o valor da velocidade no ecra
+    tempo = font.render("t=" + str(int(t)), antialias, BLACK)
+    velocidade = font.render("v=" + str(veloc(t)), antialias, BLACK) # ERRADO
+    janela.blit(pista, (0, 0))  # (B) se descomentar aqui (e comentar (A)) vejo movimento
+    janela.blit(carro_rotacao(t), parametrizacao(t))
+    janela.blit(policia, parametrizacao_policia(t))
+    janela.blit(tempo, (20, 30))
+    janela.blit(velocidade, (20, 50))  # Mostrar o valor da velocidade no ecra
     pygame.display.update()
     clock.tick(frame_rate)
-    t=t+0.1
-    
+    t = t + 0.1
 
-    
     for event in pygame.event.get():
-        #Para sair...
+        # Para sair...
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
-        #Ao clicar em qualquer local, o tempo recomeça com t=0
+        # Ao clicar em qualquer local, o tempo recomeça com t=0
         # evento mouse click botão esquerdo (código = 1)
-        elif event.type== pygame.MOUSEBUTTONUP and event.button == 1:
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
             t = 0
-                       
+            pygame.mixer.unpause()
+            motor.play()
 
-##        #Quando queremos saber as coordenadas de um ponto: 
+##        #Quando queremos saber as coordenadas de um ponto:
 ##        # descomentar isto e comentar o "evento mouse click"...
 ##        #"clicar" nesse ponto... o python print as coordenadas.
 ##        # evento mouse click botão esquerdo (código = 1)
-##        elif event.type== pygame.MOUSEBUTTONUP and event.button == 1:
+##        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
 ##            (x, y) = event.pos
-##            localizacao="posicao=(" + str(x) + "," + str(y) + ")"
+##            localizacao = "posicao=(" + str(x) + "," + str(y) + ")"
 ##            print(localizacao)
 
 
@@ -126,4 +211,5 @@ while True:
 ##            Quando parametrização (ou velocidade) não está definida
 ##            para algum valor de t, dá o erro:
 ##                "local variable "result/resultado" referenced before assignment"
-##            
+##
+
